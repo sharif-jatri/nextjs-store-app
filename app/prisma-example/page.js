140846e33b6117a2e35a0add6bcaaad20e6f92a5
sharif-jatri <108609168+sharif-jatri@users.noreply.github.com>
2024-02-20 15:55:27 +0600

    create and display data

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,7 +1,37 @@
-export default function PrismaExamplePage() {
-  return (
-      <div>
-        <h1>PrismaExamplePage</h1>
-      </div>
-  );
-}
+import prisma from '@/utils/db';
+
+const prismaHandlers = async () => {
+    console.log('prisma example');
+    // await prisma.task.create({
+    //   data: {
+    //     content: 'wake up',
+    //   },
+    // });
+    const allTasks = await prisma.task.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+    return allTasks;
+};
+
+const PrismaExample = async () => {
+    const tasks = await prismaHandlers();
+    if (tasks.length === 0) {
+        return <h2 className='mt-8 font-medium text-lg'>No tasks to show...</h2>;
+    }
+
+    return (
+        <div>
+            <h1 className='text-7xl'>PrismaExample</h1>
+            {tasks.map((task) => {
+                return (
+                    <h2 key={task.id} className='text-xl py-2'>
+                        * {task.content}
+                    </h2>
+                );
+            })}
+        </div>
+    );
+};
+export default PrismaExample;
