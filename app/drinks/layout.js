e9991c0ec54c52cde8000ce078e6025d40ed174e
sharif-jatri <108609168+sharif-jatri@users.noreply.github.com>
2024-02-20 12:07:44 +0600

    added drinks list and details with api

---
+++ app/drinks/layout.js
@@ -0,0 +1,13 @@
+const DrinksLayout = ({ children }) => {
+    return (
+        <div className='max-w-xl'>
+            <div className='mockup-code mb-8'>
+        <pre data-prefix='$'>
+          <code>Drinks</code>
+        </pre>
+            </div>
+            {children}
+        </div>
+    );
+};
+export default DrinksLayout;
